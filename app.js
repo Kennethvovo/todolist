b98faaffd7465f4d0b0c3cdf7d830fa38247a6fc
Kennethvovo <vovo811111@gmail.com>
2021-10-30 21:03:07 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const Todo = require('./models/todo')
 // 載入handlebars
 const exphbs = require('express-handlebars')
 
+// 引用 body-parser
+const bodyParser = require('body-parser')
+
 // 設定app.engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -19,6 +22,9 @@ app.get('/', (req, res) => {
     .then((todos) => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch((error) => console.error(error)) // 錯誤處理
 })
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list')
@@ -37,6 +43,23 @@ db.once('open', () => {
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/todos', (req, res) => {
+  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
+
+  // 方法一
+  return Todo.create({ name }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch((error) => console.log(error))
+  // 方法二
+  // const todo = new Todo({name})
+  // return todo.save()
+  // .then(()=>res.redirect('/'))
+  // .catch(error => console.log(error))
 })
 
 // 設定 port 3000
