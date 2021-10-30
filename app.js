a547039f7444d76fe3cffffae790cc05ad704db4
Kennethvovo <vovo811111@gmail.com>
2021-10-30 20:04:06 +0800

    feat:modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const express = require('express')
 const app = express()
 // 載入 mongoose
 const mongoose = require('mongoose')
+// 載入 Todo model
+const Todo = require('./models/todo')
+// 載入handlebars
+const exphbs = require('express-handlebars')
+
+// 設定app.engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+// 設定路由
+// Todo 首頁
+app.get('/', (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)) // 錯誤處理
+})
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list')
