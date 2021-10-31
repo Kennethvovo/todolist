6bf6f6a36fe73bcc4e18314bcaba33659097b1b2
Kennethvovo <vovo811111@gmail.com>
2021-10-31 23:52:44 +0800

    feat: fix edit can't change name

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,23 @@
+// 載入 todo model
+const Todo = require('../todo')
+
+// 載入 mongoose
 const mongoose = require('mongoose')
-const Todo = require('../todo') // 載入 todo model
-mongoose.connect('mongodb://localhost/todo-list')
+// 設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
 const db = mongoose.connection
+// 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+//新增 10 筆資料
 db.once('open', () => {
   console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
