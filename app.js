0161f632fec43e73a96b9b00e36de394fbe00afa
Kennethvovo <vovo811111@gmail.com>
2021-11-07 08:34:22 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 // 載入 express-handlebars
 const exphbs = require('express-handlebars')
-
 // 載入 Todo model
 const Todo = require('./models/todo')
 // 載入 mongoose
 const mongoose = require('mongoose')
-
+// 引用路由器
+const routes = require('./routes')
 // 引用 body-parser
 const bodyParser = require('body-parser')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
@@ -19,10 +19,8 @@ const db = mongoose.connection
 const methodOverride = require('method-override')
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -34,65 +32,9 @@ db.once('open', () => {
 //樣板引擎指定為 Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 // 設定首頁路由
-app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' }) // 新增這裡：根據 _id 升冪排序
-    .then((todos) => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch((error) => console.error(error)) // 錯誤處理
-})
-// 設定new
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-// 設定create路由
-app.post('/todos', (req, res) => {
-  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name }) // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch((error) => console.log(error))
-})
-// 設定detail路由
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch((error) => console.log(error))
-})
-// 設定edit路由
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch((error) => console.log(error))
-})
-// 設定edit路由name修改後
-app.put('/todos/:id/', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then((todo) => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch((error) => console.log(error))
-})
-// 設定delete路由
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then((todo) => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch((error) => console.log(error))
-})
-
+// 將 request 導入路由器
+app.use(routes)
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
