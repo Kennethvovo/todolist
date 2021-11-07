8dd8b77e954236eba4aceb38139c47a430a3063e
Kennethvovo <vovo811111@gmail.com>
2021-11-07 08:00:32 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -13,12 +13,16 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 載入 method-override
+const methodOverride = require('method-override')
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -68,7 +72,7 @@ app.get('/todos/:id/edit', (req, res) => {
     .catch((error) => console.log(error))
 })
 // 設定edit路由name修改後
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id/', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -81,7 +85,7 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch((error) => console.log(error))
 })
 // 設定delete路由
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then((todo) => todo.remove())
