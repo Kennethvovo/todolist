359d2ce9e358accd3914f4129939202de9fc2867
Kennethvovo <vovo811111@gmail.com>
2021-11-07 13:02:15 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
 // 載入 express-handlebars
 const exphbs = require('express-handlebars')
 // 載入 Todo model
 const Todo = require('./models/todo')
-// 載入 mongoose
-const mongoose = require('mongoose')
-// 引用路由器
-const routes = require('./routes')
 // 引用 body-parser
 const bodyParser = require('body-parser')
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
-// 取得資料庫連線狀態
-const db = mongoose.connection
 // 載入 method-override
 const methodOverride = require('method-override')
+// 引用路由器
+const routes = require('./routes')
+
+const app = express()
+require('./config/mongoose')
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 //樣板引擎指定為 Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-// 設定首頁路由
-// 將 request 導入路由器
+// 設定首頁路由 將 request 導入路由器
 app.use(routes)
 // 設定 port 3000
 app.listen(3000, () => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,25 +1,6 @@
-// 載入 todo model
 const Todo = require('../todo')
-
-// 載入 mongoose
-const mongoose = require('mongoose')
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
+const db = require('../../config/mongoose')
 db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-//新增 10 筆資料
-db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: 'name-' + i })
   }
