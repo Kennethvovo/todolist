81389a74094445bbcea90707ba957853d625f6ee
Kennethvovo <vovo811111@gmail.com>
2022-01-06 22:28:45 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
 // 引用路由器
 const routes = require('./routes')
 const { prototype } = require('express-handlebars/lib/express-handlebars')
