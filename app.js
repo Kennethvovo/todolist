9265970c30cb68e071f44a6aa2ae27d8a8aff0e4
Kennethvovo <vovo811111@gmail.com>
2022-01-07 21:03:06 +0800

    feat: add user authentication in todo and home routers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const session = require('express-session')
 // 載入 express-handlebars
 const exphbs = require('express-handlebars')
 // 載入 Todo model
@@ -11,21 +12,26 @@ const methodOverride = require('method-override')
 
 // 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
-// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 
 // 引用路由器
 const routes = require('./routes')
 const { prototype } = require('express-handlebars/lib/express-handlebars')
 const PORT = process.env.PORT || 3000
-const session = require('express-session')
-const app = express()
 
+const app = express()
 require('./config/mongoose')
+
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
+//樣板引擎指定為 Handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+usePassport(app)
 app.use(
   session({
     secret: 'ThisIsMySecret',
@@ -33,12 +39,6 @@ app.use(
     saveUninitialized: true,
   })
 )
-
-//樣板引擎指定為 Handlebars
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-usePassport(app)
-
 // 設定首頁路由 將 request 導入路由器
 app.use(routes)
 // 設定 port 3000
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,8 @@ const router = express.Router()
 const todos = require('./modules/todos')
 // 引入 home 模組程式碼
 const home = require('./modules/home')
-
 const users = require('./modules/users')
 const { authenticator } = require('../middleware/auth') // 掛載 middleware
-
 router.use('/todos', authenticator, todos) // 加入驗證程序
 router.use('/users', users)
 router.use('/', authenticator, home) // 加入驗證程序
